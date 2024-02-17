78e15d8ca99407dfe0fdd148d7b0c5d1bc47a1e2
Adwait S Purao <adwait.purao@spit.ac.in>
2024-02-18 00:10:42 +0530

    Delete client directory

--- client/src/Store.js
+++
@@ -1,9 +0,0 @@
-// import { createStore } from 'redux';
-
-// const initialState = {
-//   loggedIn: false,
-// };
-
-// const store = createStore(reducer, initialState);
-
-// export default store;

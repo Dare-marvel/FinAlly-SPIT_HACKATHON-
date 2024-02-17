798f5c0c48704a1545d91bf8a3db2745336983a5
Adwait S Purao <adwait.purao@spit.ac.in>
2024-02-18 00:10:50 +0530

    Delete server directory

--- server/models/PricesModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const priceSchema = new mongoose.Schema({
-    item: String,
-    price: String,
-  });
-  
-  const PriceModel = mongoose.model('Price', priceSchema);
-  module.exports = PriceModel;
